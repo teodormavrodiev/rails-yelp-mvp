dad2498bf805530e02ffdc3554298179a62d0ee3
Teodor Mavrodiev <teodor.mavrodiev@gmail.com>
2017-02-22 14:30:12 +0100

    added validations and specified routes needed

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+  validates :restaurant_id, presence: true
+  validates :content, presence: true
+  validates :rating, presence: true, inclusion: {in: [0,1,2,3,4,5]}, numericality: { only_integer: true }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :reviews
-  resources :restaurants
+  resources :restaurants, except: [:destroy, :edit, :update] do
+    resources :reviews, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
